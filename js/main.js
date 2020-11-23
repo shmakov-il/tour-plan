ee4807f10de793ecd661a229e6d0b571ea27894b
shmakov-il <ilia.eduardovich98@gmail.com>
2020-11-23 20:25:05 +0300

    create: added navigation keyboard

--- js/main.js
+++ js/main.js
@@ -7,4 +7,9 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  // Navigation keyboard
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 })
