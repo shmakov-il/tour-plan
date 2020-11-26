4b72b0137588e53decc9e9ef746cc1e8c2a217fb
shmakov-il <ilia.eduardovich98@gmail.com>
2020-11-26 11:42:47 +0300

    create: added section newsletter and reviews

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   // Navigation keyboard
   keyboard: {
@@ -14,4 +14,21 @@ var mySwiper = new Swiper('.swiper-container', {
   },
   speed: 2000,
   effect: 'coverflow',
+});
+
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  // Navigation keyboard
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 })
